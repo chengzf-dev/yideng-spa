77e50f2b19f7f9456aef294581234d40c122c1dd
chengzf-dev <czf881009@163.com>
2025-12-07 02:48:59 +0800

    fix: husky config

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,16 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-if [ -z "$husky_skip_init" ]; then
-  export husky_skip_init=1
-
-  command_path="$(command -v sh)"
-  if [ -n "$command_path" ]; then
-    command "$command_path" "$0" "$@"
-    exit $?
-  fi
-
-  if command -v sh >/dev/null 2>&1; then
-    sh "$0" "$@"
-    exit $?
-  fi
-fi
+They WILL FAIL in v10.0.0
+"
